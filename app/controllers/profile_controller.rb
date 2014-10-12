3bfbac859a5f4acea9fe01e0e435c5f0eaf489e3
Jason Blanchard <jas.blanchard@gmail.com>
2014-10-12 20:23:39 +0000

    Added gravatar

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,8 @@ class ProfileController < ApplicationController
   def show
     @user = User.find(params[:id])
     @twets = @user.twets.order('created_at DESC')
+    @gravatar_hash = Digest::MD5.hexdigest(@user.email.downcase)
+    @profile_img_src = "http://www.gravatar.com/avatar/#{@gravatar_hash}"
   end
   
 end
