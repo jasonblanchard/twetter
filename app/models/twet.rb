4aa9f3874167e533fab5a11dde92c7a879197a1a
Jason Blanchard <jas.blanchard@gmail.com>
2014-10-12 13:33:34 -0400

    Spec passing from after_save in model

--- app/models/twet.rb
+++ app/models/twet.rb
@@ -4,10 +4,19 @@ class Twet < ActiveRecord::Base
   validates :content, :presence => true, :length => { :minimum => 2, :maximum => 140 }
   validates :user, :presence => true
 
+  after_save :send_notification
+
   # Gets all twets made by the users referenced by the ids passed, starting with the
   # most recent twet made.
   #
   def self.by_user_ids(*ids)
     where(:user_id => ids.flatten.compact.uniq).order('created_at DESC')
+  end
+
+  private
+
+  def send_notification
+    @recipients = Follow.where(:following_id => self.user.id).map(&:user)
+    TwetNotification.new_twet_from_follower(self.user, @recipients).deliver
   end
 end
